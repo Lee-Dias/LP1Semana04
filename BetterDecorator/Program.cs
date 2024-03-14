7fdd963c75288dcfb43d16cd047c8b6c72746ffe
lee-dias <leedias.dev@gmail.com>
2024-03-14 18:16:45 +0000

    add do BeterDecorator com o codigo igual ao slide anterior

---
+++ BetterDecorator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BetterDecorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Decor( "Good Morning ! " , '∗' ));
+            Console.WriteLine(Decor( "Good E v e ni n g ! " , '.' ) );
+
+        }
+
+        private static string Decor(string s, char dec)
+        {
+            return $"{dec}{dec}{dec} {s} {dec}{dec}{dec}";     
+        }
+        
+    }
+}
