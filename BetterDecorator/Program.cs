6e742d2ce4d5d86e74b0f988f2836b39173aedab
lee-dias <leedias.dev@gmail.com>
2024-03-14 18:51:16 +0000

    update BeterDecorator correção no codigo do Decor

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -7,22 +7,25 @@ namespace BetterDecorator
         static void Main(string[] args)
         {
             char o = char.Parse(args[1]);
-            int a = int.Parse(args[1]);
-            Decor(args[0],o,a);
+            
+            int a = int.Parse(args[2]);
+
+
+            Console.WriteLine(Decor(args[0],o,a));
         }
 
         private static string Decor(string s, char dec, int i)
         {
             string a = "";
             string b = "";
-            for (int k = 0; k == i; k++){
-                if(k > (i/2)){
+            for (int k = 0; k < i; k++){
                     a = a +dec; 
-                }else{
-                    b = b +dec; 
-
-                }
+                
             }
+            for (int k = 0; k < i; k++){
+                    b = b+dec; 
+            }
+                
             return $"{a} {s} {b}";     
         }
         
