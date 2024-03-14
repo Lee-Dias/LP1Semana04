e12fbd9bba63baa82379b354ccca57959b0e20b6
lee-dias <leedias.dev@gmail.com>
2024-03-14 19:26:55 +0000

    add Special com Getspecial a funcionar

---
+++ Special/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Special
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        private static int GetSpecial( int n){
+            int p = 0;
+            int u = 1;
+            int t = 0;
+            for(int k = 0; k <= n; k++){
+              t = u + 2*p;
+              p= u;
+              u = t;
+            }
+            return t;
+        }
+
+    }
+}
