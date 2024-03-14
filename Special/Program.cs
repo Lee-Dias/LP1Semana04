6197d1a779fa5fe2a2d5029937d370cec4e822a0
lee-dias <leedias.dev@gmail.com>
2024-03-14 19:32:28 +0000

    add Special com Getspecial a funcionar melhor e com o main a pedir o numero ao utilizador

--- Special/Program.cs
+++ Special/Program.cs
@@ -7,13 +7,18 @@ namespace Special
     {
         static void Main(string[] args)
         {
-
+            Console.Write("Insert the number of the special sequence: ");
+            int i = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetSpecial(i));
         }
         private static int GetSpecial( int n){
             int p = 0;
             int u = 1;
             int t = 0;
-            for(int k = 0; k <= n; k++){
+            if (n == 1){
+                t = 1;
+            }
+            for(int k = 2; k <= n; k++){
               t = u + 2*p;
               p= u;
               u = t;
